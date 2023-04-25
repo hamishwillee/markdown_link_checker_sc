7aefbaf957ed71be0f5222fc8f2258e379022a53
Hamish Willee <hamishwillee@gmail.com>
2023-04-26 08:55:31 +1000

    Split out code for finding orphan pages

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ import { processMarkdown } from "./src/process_markdown.js";
 import { processRelativeLinks } from "./src/process_relative_links.js";
 import { processLocalImageLinks } from "./src/process_local_image_links.js";
 import { processUrlsToLocalSource } from "./src/process_internal_url_links.js";
+import { checkSummaryOrphans, getPageWithMostLinks } from "./src/process_orphans.js";
+
 
 program
   .option(
@@ -146,147 +148,7 @@ const processDirectory = async (dir, options) => {
   return results;
 };
 
-// Gets page with most links. Supposed to be used on the allResults object that is an array of objects about each page.
-// Will use to get the summary.
-function getPageWithMostLinks(pages) {
-  if (options.log.includes("functions")) {
-    console.log("Function: getPageWithMostLinks");
-  }
-  return pages.reduce(
-    (maxLinksPage, currentPage) => {
-      if (
-        currentPage.relativeLinks.length > maxLinksPage.relativeLinks.length
-      ) {
-        return currentPage;
-      } else {
-        return maxLinksPage;
-      }
-    },
-    { relativeLinks: [] }
-  ).page_file;
-}
 
-// Get any orphans (no links from summary and no links at all)
-//
-function checkSummaryOrphans(results) {
-  const resultObj = {};
-  const allInternalAbsLinks = [];
-
-  //Create result object that has page as property
-  // And value is an array of links in/from that page converted to absolute.
-  results.forEach((obj) => {
-    const filePath = obj.page_file;
-    const relativeLinks = obj.relativeLinks;
-    const absLinks = [];
-
-    relativeLinks.forEach((linkObj) => {
-      const linkUrl = linkObj.linkUrl;
-      const absLink = path.resolve(path.dirname(filePath), linkUrl);
-      absLinks.push(absLink);
-      allInternalAbsLinks.push(absLink);
-    });
-
-    resultObj[filePath] = absLinks;
-  });
-
-  // Invert resultObj to get all objects to link to page.
-  // Add the links to to the big results object we process later.
-  const pagesObj = {};
-  for (const [page, links] of Object.entries(resultObj)) {
-    for (const link of links) {
-      if (!pagesObj[link]) {
-        pagesObj[link] = [];
-      }
-      pagesObj[link].push(page);
-    }
-  }
-  results.forEach((obj) => {
-    obj["linkedFrom"] = pagesObj[obj.page_file];
-  });
-
-  // Check that every filepath has at least one object in some absLink that matches it
-  let allFilesReferenced = true;
-  let allFilesSummaryReferenced = true;
-  const allFilesNoReference = [];
-  const allFilesNoSummaryReference = [];
-  results.forEach((obj) => {
-    const filePath = obj.page_file;
-    if (!allInternalAbsLinks.some((absLink) => absLink === filePath)) {
-      if (obj.redirectTo) {
-        //do nothing
-      } else if (obj.page_file === options.toc) {
-        //do nothing
-      } else {
-        //if it a redirect file then it shouldn't be linked.
-        allFilesNoReference.push(filePath);
-        //console.log(`File "${filePath}" not referenced by any absolute link`);
-        const error = {
-          type: "PageNotLinkedInternally",
-          page: `${obj.page_file}`,
-        };
-        results.allErrors.push(error);
-        allFilesReferenced = false;
-      }
-    }
-
-    const summaryFileLinks = resultObj[options.toc];
-
-    if (!summaryFileLinks.some((absLink) => absLink === filePath)) {
-      if (obj.redirectTo) {
-        // do nothing
-      } else if (obj.page_file === options.toc) {
-        //do nothing
-      } else {
-        //if it a redirect file then it shouldn't be linked.
-        allFilesNoSummaryReference.push(filePath);
-        //console.log(`File "${filePath}" not referenced in summary.md`);
-        const error = {
-          type: "PageNotLinkedFromSummary",
-          page: `${obj.page_file}`,
-        };
-        if (!results.allErrors) {
-          results["allErrors"] = [];
-        }
-        results.allErrors.push(error);
-        allFilesSummaryReferenced = false;
-      }
-    }
-  });
-
-  if (!allFilesReferenced) {
-    const jsonAllFilesNotReferenced = JSON.stringify(
-      allFilesNoReference,
-      null,
-      2
-    );
-    logToFile("./logs/allFilesNoReference.json", jsonAllFilesNotReferenced);
-  } else {
-    //console.log("All files referenced at least once");
-  }
-
-  if (!allFilesSummaryReferenced) {
-    const jsonAllFilesNotSummaryReferenced = JSON.stringify(
-      allFilesNoSummaryReference,
-      null,
-      2
-    );
-    logToFile(
-      "./logs/allFilesNoSummaryReference.json",
-      jsonAllFilesNotSummaryReferenced
-    );
-  } else {
-    //console.log("All files referenced at least once");
-  }
-
-  if (options.log.includes("quick")) {
-    //console.log(resultObj);
-    const jsonFilesWithAbsoluteLinks = JSON.stringify(resultObj, null, 2);
-    logToFile(
-      "./logs/pagesResolvedAbsoluteLinks.json",
-      jsonFilesWithAbsoluteLinks
-    );
-  }
-}
 
 function filterErrors(errors) {
   // This method filters all errors against settings in the command line - such as pages to output.
@@ -334,8 +196,8 @@ function filterErrors(errors) {
   results["allErrors"].push(...errorsFromUrlsToLocalSite);
 
   //Can now guess the summary file if not specified
-  options.toc ? null : (options.toc = getPageWithMostLinks(results));
-  checkSummaryOrphans(results);
+  options.toc ? null : (options.toc = getPageWithMostLinks(results, options));
+  checkSummaryOrphans(results, options);
   const filteredResults = filterErrors(results.allErrors);
 
   outputErrors(filteredResults, options);
---
+++ src/process_orphans.js
@@ -0,0 +1,146 @@
+import { logToFile } from "./helpers.js";
+import path from "path";
+
+// Gets page with most links. Supposed to be used on the allResults object that is an array of objects about each page.
+// Will use to get the summary.
+function getPageWithMostLinks(pages, options) {
+  if (options.log.includes("functions")) {
+    console.log("Function: getPageWithMostLinks");
+  }
+  return pages.reduce(
+    (maxLinksPage, currentPage) => {
+      if (
+        currentPage.relativeLinks.length > maxLinksPage.relativeLinks.length
+      ) {
+        return currentPage;
+      } else {
+        return maxLinksPage;
+      }
+    },
+    { relativeLinks: [] }
+  ).page_file;
+}
+
+// Get any orphans (no links from summary and no links at all)
+//
+function checkSummaryOrphans(results, options) {
+  const resultObj = {};
+  const allInternalAbsLinks = [];
+
+  //Create result object that has page as property
+  // And value is an array of links in/from that page converted to absolute.
+  results.forEach((obj) => {
+    const filePath = obj.page_file;
+    const relativeLinks = obj.relativeLinks;
+    const absLinks = [];
+
+    relativeLinks.forEach((linkObj) => {
+      const linkUrl = linkObj.linkUrl;
+      const absLink = path.resolve(path.dirname(filePath), linkUrl);
+      absLinks.push(absLink);
+      allInternalAbsLinks.push(absLink);
+    });
+
+    resultObj[filePath] = absLinks;
+  });
+
+  // Invert resultObj to get all objects to link to page.
+  // Add the links to to the big results object we process later.
+  const pagesObj = {};
+  for (const [page, links] of Object.entries(resultObj)) {
+    for (const link of links) {
+      if (!pagesObj[link]) {
+        pagesObj[link] = [];
+      }
+      pagesObj[link].push(page);
+    }
+  }
+  results.forEach((obj) => {
+    obj["linkedFrom"] = pagesObj[obj.page_file];
+  });
+
+  // Check that every filepath has at least one object in some absLink that matches it
+  let allFilesReferenced = true;
+  let allFilesSummaryReferenced = true;
+  const allFilesNoReference = [];
+  const allFilesNoSummaryReference = [];
+  results.forEach((obj) => {
+    const filePath = obj.page_file;
+    if (!allInternalAbsLinks.some((absLink) => absLink === filePath)) {
+      if (obj.redirectTo) {
+        //do nothing
+      } else if (obj.page_file === options.toc) {
+        //do nothing
+      } else {
+        //if it a redirect file then it shouldn't be linked.
+        allFilesNoReference.push(filePath);
+        //console.log(`File "${filePath}" not referenced by any absolute link`);
+        const error = {
+          type: "PageNotLinkedInternally",
+          page: `${obj.page_file}`,
+        };
+        results.allErrors.push(error);
+        allFilesReferenced = false;
+      }
+    }
+
+    const summaryFileLinks = resultObj[options.toc];
+
+    if (!summaryFileLinks.some((absLink) => absLink === filePath)) {
+      if (obj.redirectTo) {
+        // do nothing
+      } else if (obj.page_file === options.toc) {
+        //do nothing
+      } else {
+        //if it a redirect file then it shouldn't be linked.
+        allFilesNoSummaryReference.push(filePath);
+        //console.log(`File "${filePath}" not referenced in summary.md`);
+        const error = {
+          type: "PageNotLinkedFromSummary",
+          page: `${obj.page_file}`,
+        };
+        if (!results.allErrors) {
+          results["allErrors"] = [];
+        }
+        results.allErrors.push(error);
+        allFilesSummaryReferenced = false;
+      }
+    }
+  });
+
+  if (!allFilesReferenced) {
+    const jsonAllFilesNotReferenced = JSON.stringify(
+      allFilesNoReference,
+      null,
+      2
+    );
+    logToFile("./logs/allFilesNoReference.json", jsonAllFilesNotReferenced);
+  } else {
+    //console.log("All files referenced at least once");
+  }
+
+  if (!allFilesSummaryReferenced) {
+    const jsonAllFilesNotSummaryReferenced = JSON.stringify(
+      allFilesNoSummaryReference,
+      null,
+      2
+    );
+    logToFile(
+      "./logs/allFilesNoSummaryReference.json",
+      jsonAllFilesNotSummaryReferenced
+    );
+  } else {
+    //console.log("All files referenced at least once");
+  }
+
+  if (options.log.includes("quick")) {
+    //console.log(resultObj);
+    const jsonFilesWithAbsoluteLinks = JSON.stringify(resultObj, null, 2);
+    logToFile(
+      "./logs/pagesResolvedAbsoluteLinks.json",
+      jsonFilesWithAbsoluteLinks
+    );
+  }
+}
+
+export { checkSummaryOrphans, getPageWithMostLinks };
