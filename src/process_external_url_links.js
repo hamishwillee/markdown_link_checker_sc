470568c131cf82c79a1ac796883ae916dbc85566
Hamish Willee <hamishwillee@gmail.com>
2025-07-10 12:23:43 +1000

    Report error only once

--- src/process_external_url_links.js
+++ src/process_external_url_links.js
@@ -36,6 +36,9 @@ async function getHeadRequestStatusCode(urlString, timeoutMs = 5000) {
     headers: {
       "User-Agent":
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
+      Accept:
+        "text/html, application/xhtml+xml;q=0.9, application/vnd.wap.xhtml+xml;q=0.6, */*;q=0.5",
+      "Accept-Language": "en-US,en;q=0.9,es;q=0.8",
     },
   };
 
@@ -394,7 +397,7 @@ async function processExternalUrlLinks(results) {
             error: urlResult.error,
           });
           errors.push(error);
-          error.output();
+          //error.output();
         }
       }
 
